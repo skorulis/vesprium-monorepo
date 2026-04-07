9103c3829866d88135952da91febc84de11fff36
Alex Skorulis <skorulis@gmail.com>
2026-04-08 08:47:36 +1000

    Start events

--- ios/VespriumHackChoice/VespriumHackChoice/Model/Event/GameEvent.swift
+++ ios/VespriumHackChoice/VespriumHackChoice/Model/Event/GameEvent.swift
@@ -5,4 +5,15 @@ import Foundation
 struct GameEvent: Sendable, Codable {
     let text: String
     let cards: [GameCard]
+    let skippable: Bool
+    
+    init(
+        text: String,
+        cards: [GameCard],
+        skippable: Bool = true
+    ) {
+        self.text = text
+        self.cards = cards
+        self.skippable = skippable
+    }
 }
