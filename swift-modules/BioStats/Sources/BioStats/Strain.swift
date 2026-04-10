a139b12ad09b4eb49e368835dd8e7a561cbc1b0f
Alex Skorulis <skorulis@gmail.com>
2026-04-10 10:22:50 +1000

    Add strain

---
+++ swift-modules/BioStats/Sources/BioStats/Strain.swift
@@ -0,0 +1,14 @@
+//  Created by Alexander Skorulis on 10/4/2026.
+
+import Foundation
+
+/// Strain represents the toll that is being applied to the body by actions or implants
+public struct Strain: Equatable, Sendable {
+    public let physical: Int
+    public let mental: Int
+    
+    public init(physical: Int, mental: Int) {
+        self.physical = physical
+        self.mental = mental
+    }
+}
